abd5eccbf69716ec070647369ad0fa2bd8bde6da
SWIN91 <88003529+SWIN91@users.noreply.github.com>
2021-10-06 09:27:02 -0500

    first commit, not finished

---
+++ objectprototype.js
@@ -0,0 +1,11 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    printInfo(){
+        return `${this.name} is ${this.age}`
+    }
+}
+let folks= new Person('Bijuu Mike', 29)
+console.log(folks);
